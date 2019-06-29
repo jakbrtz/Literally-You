7b431d4e1f20735cfba0be2191d3be1f83c411e5
Jake <jake@bortz.com.au>
2019-06-29 14:45:18 +1000

    give discord user more control

--- bot.py
+++ bot.py
@@ -1,7 +1,10 @@
 import discord
 from repeater import owo
 from repeater import clap
-from repeater import lisp
+from repeater import dobby
+
+channels = {}
+modes = {}
 
 # Everything that the bot does is inside this class:
 class MyClient(discord.Client):
@@ -15,10 +18,29 @@ class MyClient(discord.Client):
 		# if the author of the message was a bot don't respond
 		if message.author.bot:
 			return
+		if message.content == '':
+			return
 		
-		await message.channel.send(message.author.name + ": " + owo(message.content))
-		await message.channel.send(message.author.name + ": " + clap(message.content))
-		await message.channel.send(message.author.name + ": " + lisp(message.content))
+		if message.channel.guild.id in channels:
+			if channels[message.channel.guild.id] == message.channel.id:
+				await message.delete()
+				if modes[message.channel.guild.id] == "clap":
+					await message.channel.send(message.author.name + ": " + clap(message.content))
+				elif modes[message.channel.guild.id] == "dobby":
+					await message.channel.send(message.author.name + ": " + dobby(message.content, message.author.name))
+				else:
+					await message.channel.send(message.author.name + ": " + owo(message.content))
+		
+		if message.content[:10] == "UwUchannel":
+			channelint = message.channel.id
+			channelint = int(message.content[13: len(message.content)-1])
+			channels[message.channel.guild.id] = channelint
+			modes[message.channel.guild.id] = 'uwu'
+			await message.channel.send("Chosen channel set to <#" + channelint + ">")
+		
+		if message.content[:7] == "UwUmode":
+			modes[message.channel.guild.id] = message.content.lower()[8:]
+			await message.channel.send("Mode set to " + message.content.lower()[8:])
 
 # Run the bot
 client = MyClient()
