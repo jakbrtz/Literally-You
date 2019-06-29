4e45519bf029c9a8c39c8df7bb2a126acf396986
Jake <jake@bortz.com.au>
2019-06-29 15:27:40 +1000

    uses pickle to remember preferences

--- bot.py
+++ bot.py
@@ -1,10 +1,20 @@
 import discord
+import pickle
 from repeater import owo
 from repeater import clap
 from repeater import dobby
 
-channels = {}
-modes = {}
+try:
+	with open('channels.pkl', 'rb') as f:
+		channels = pickle.load(f)
+except:
+	channels = {}
+
+try:
+	with open('modes.pkl', 'rb') as f:
+		modes = pickle.load(f)
+except:
+	modes = {}
 
 # Everything that the bot does is inside this class:
 class MyClient(discord.Client):
@@ -24,9 +34,13 @@ class MyClient(discord.Client):
 		if message.channel.guild.id in channels:
 			if channels[message.channel.guild.id] == message.channel.id:
 				await message.delete()
-				if modes[message.channel.guild.id] == "clap":
+				try:
+					mode = modes[message.channel.guild.id]
+				except:
+					mode = "uwu"
+				if mode == "clap":
 					await message.channel.send("**" + message.author.name + ":** " + clap(message.content))
-				elif modes[message.channel.guild.id] == "dobby":
+				elif mode == "dobby":
 					await message.channel.send("**" + message.author.name + ":** " + dobby(message.content, message.author.name))
 				else:
 					await message.channel.send("**" + message.author.name + ":** " + owo(message.content))
@@ -35,12 +49,17 @@ class MyClient(discord.Client):
 			channelint = message.channel.id
 			channelint = int(message.content[13: len(message.content)-1])
 			channels[message.channel.guild.id] = channelint
-			modes[message.channel.guild.id] = 'uwu'
+			if not message.channel.guild.id in modes:
+				modes[message.channel.guild.id] = 'uwu'
+			with open('channels.pkl', 'wb') as f:
+				pickle.dump(channels, f, pickle.HIGHEST_PROTOCOL)
 			await message.channel.send("I will now lurk in <#" + str(channelint) + ">")
 			await message.channel.guild.me.edit(nick = "Literally")
 		
 		if message.content[:7] == "UwUmode":
 			modes[message.channel.guild.id] = message.content.lower()[8:]
+			with open('modes.pkl', 'wb') as f:
+				pickle.dump(modes, f, pickle.HIGHEST_PROTOCOL)
 			await message.channel.send("Mode set to " + message.content.lower()[8:])
 
 # Run the bot
